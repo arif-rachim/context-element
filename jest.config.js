177de0f747e97f06ba5e40e333a5c953ab8fcd15
arif <marsa.emreef@gmail.com>
2020-06-24 10:26:58 +0400

    ci(update travis to support codecov): update travis to support codecov
    Update travis.yml to support codecov report

--- jest.config.js
+++ jest.config.js
@@ -3,6 +3,7 @@ module.exports = {
     transform: {
         '^.+\\.tsx?$': 'ts-jest',
     },
+    collectCoverage:true,
     testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$',
     moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
 }
